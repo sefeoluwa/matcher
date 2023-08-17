787c41c0c700f494e34e00ec64052eb7ffbe905e
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-17 09:33:20 +0100

    navLinks export

---
+++ src/constants/index.js
@@ -0,0 +1,23 @@
+import {
+
+
+} from '../assets';
+
+const navLinks = [
+{
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "pricing",
+    title: "Pricing",
+  },
+  {
+    id: 'about',
+    title: 'About Us',
+  },
+];
+
+export {
+    navLinks,
+}
