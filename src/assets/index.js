c68f406e04843d7cf349e3bfe3a9f8842cffd7e4
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-17 13:05:46 +0100

    added new assets

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import header from './header.png'
+import logo from './logo.jpg'
+import menu from './menu.svg'
+import close from './close.svg'
+
+export {
+    logo,
+    header,
+    menu,
+    close,
+}
