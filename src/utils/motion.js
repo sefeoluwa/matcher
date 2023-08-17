a7f47d2b6d5f7cf8ba8c9c7d4ba7f74b0c58c327
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-17 15:22:22 +0100

    added new variants

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -86,3 +86,25 @@ export const textVariant = (delay) => {
       },
     };
   };
+
+  export const itemVariants = {
+    closed: {
+      opacity: 0
+    },
+    open: { opacity: 1 }
+  };
+  
+ export const sideVariants = {
+    closed: {
+      transition: {
+        staggerChildren: 0.2,
+        staggerDirection: -1
+      }
+    },
+    open: {
+      transition: {
+        staggerChildren: 0.2,
+        staggerDirection: 1
+      }
+    }
+  };
