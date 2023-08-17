5fbf3ed73f1c1ac398b6ec8657869b65a348a7a9
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-17 15:22:36 +0100

    added bg-secondary

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     extend: {
       colors: {
         primary: "#050816",
-        secondary: "#aaa6c3",
+        secondary: "#8A2BE2",
         tertiary: "#151030",
         "black-100": "#100d25",
         "black-200": "#090325",
