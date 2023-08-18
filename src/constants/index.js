e163324dbaf3d10cc9547ea7291bf3dd23affd1d
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-18 23:38:25 +0100

    added some neccessary constants :fire:

--- src/constants/index.js
+++ src/constants/index.js
@@ -18,6 +18,97 @@ const navLinks = [
   },
 ];
 
+const features = [
+  {
+    icon: '',
+    title: "Simple",
+    content: "Creating a powerful resume shouldn't feel like rocket science. Resume Matcher tool redefines simplicity, making the process accessible to everyone on the job search journey.",
+  },
+  {
+    icon: '',
+    title: "ATS friendly",
+    content: "Resume Matcher is your ally in navigating the digital screening process. Optimze your resume with the right features to pass the ATS keyword scanner.",
+  },
+  {
+    icon: '',
+    title: "Awesome design",
+    content: "Say goodbye to generic templates. We offer a collection of beautifully crafted designs that allow your resume to stand out in a sea of monotonous documents."
+  }
+]
+
+const howItWorks = [
+  {
+    title: 'Step 1',
+    content: 'Upload your resume.'
+  },
+  {
+    title: 'Step 2',
+    content: 'Enter job description.'
+  },
+  {
+    title: 'Step 3',
+    content: 'AI powered analytics and resume review.'
+  },
+  {
+    title: 'Step 4',
+    content: 'Algorithm matches resume to job description.'
+  },
+  {
+    title: 'Step 5',
+    content: 'Download optimized resume.'
+  },
+]
+
+const aboutUs = [
+  {
+    image: '',
+    person: 'name',
+    about: 'some details'
+  },
+  {
+    image: '',
+    person: 'name',
+    about: 'some details'
+  },
+]
+
+const footerData = [
+  {
+    title: 'Resume Matcher',
+    list: [
+      "Github Apache 2.0",
+    ]
+  },
+  {
+    title: 'Community',
+    list: [
+      'Dev Community Post',
+    ],
+  },
+  {
+    title: 'Find Us',
+    list: [
+      "Product Hunt",
+      "Discord",
+      'Alternative To',
+      'Toolify AI',
+    ],
+  },
+  {
+    title: 'GitHub',
+    list: [
+      'Creator',
+      'Contributors',
+      'Star Gazers',
+      'Forks'
+    ]
+  }
+]
+
 export {
     navLinks,
+    features,
+    howItWorks,
+    aboutUs,
+    footerData,
 }
