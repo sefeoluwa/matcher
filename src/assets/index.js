c17b6328e0d8e041449ab08beee91c1fffa848e1
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-18 15:39:21 +0100

    added dummy gif

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,10 +2,12 @@ import header from './header.png'
 import logo from './logo.jpg'
 import menu from './menu.svg'
 import close from './close.svg'
+import giphy from './giphy.gif'
 
 export {
     logo,
     header,
     menu,
     close,
+    giphy,
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { SectionWrapper }from '../hoc'
+import { styles } from '../styles'
+
 
 function Hero() {
   return (
-    <div>Hero</div>
+   <>
+   <div className='hero-container flex flex-col  items-center pt-[10vh] h-[70vh]'>
+    <h1 className={`${styles.heroHeadText} hero-text`}>Resume Matcher</h1>
+    <p className={`${styles.heroSubText} hero-text text-center`}>Optimise your resume with AI for ATS and get hired.</p>
+    <button>Try it for free!</button>
+   </div>
+   </>
   )
 }
 
-export default Hero
+export default SectionWrapper(Hero, '')
