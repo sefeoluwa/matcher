b37a04d1f5d0d074bf98838ba2f719efa2f83b0a
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-19 22:18:50 +0100

    text aligned center

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ function Hero() {
    <>
    <div className="hero  pt-[10vh] h-[80vh]">
    <div className='hero-container flex flex-col  items-center '>
-    <h1 className={`${styles.heroHeadText} hero-text`}>Resume Matcher</h1>
+    <h1 className={`${styles.heroHeadText} hero-text text-center`}>Resume Matcher</h1>
     <p className={`${styles.heroSubText} hero-text text-center`}>Optimise your resume with AI for ATS and get hired!</p>
     <button className='bg-[#8A2BE2] w-[200px] h-[46px] rounded-[36px] mt-6'>Try it for free!</button>
    </div>
