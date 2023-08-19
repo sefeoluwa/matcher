500aa877cdfa712bbb5af97b5f1100c8d8502912
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-19 22:18:26 +0100

    styles for features-card

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -8,14 +8,14 @@ function Features() {
   return (
     <>
     <div className="heading">
-      <h2>Powerful features to help you pass the screening stage amd get hired faster.</h2>
+      <h2 className='font-bold text-[30px]'>Powerful features to help you pass the screening stage amd get hired faster.</h2>
     </div>
-    <div className="">
+    <div className="flex flex-wrap justify-around w-full">
       {features.map((feat) => (
-        <div className="" key={feat.title}>
+        <div className="features-card mt-5" key={feat.title}>
           <img src={feat.icon} alt="" />
-          <h3>{feat.title}</h3>
-          <p>{feat.content}</p>
+          <h3 className='text-[25px] font-bold '>{feat.title}</h3>
+          <p className='text-[18px]'>{feat.content}</p>
         </div>
       ))}
     </div>
