0985f4c64a30f5f2497a3bcb86c728ca361b8383
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-19 23:52:35 +0100

    created vertical timeline with how it works steps

--- src/components/How.jsx
+++ src/components/How.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
+import { SectionWrapper } from '../hoc'
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
+import "react-vertical-timeline-component/style.min.css";
+import { styles } from '../styles';
+import { howItWorks } from '../constants';
+import { textVariant } from "../utils/motion";
 
-function How() {
+const HowSteps = ({ howItWorks }) => {
   return (
-    <div>How</div>
+    <VerticalTimelineElement contentStyle={{background: '#1d1836', color: '#fff'}}
+    contentArrowStyle={{ borderRight:'7px solid #232631'}}
+    >
+       <div>
+          <img src={howItWorks.icon} alt="" />
+           <h3 >{howItWorks.title}</h3>
+           <p>{howItWorks.content}</p>
+         </div>
+       </VerticalTimelineElement>
   )
 }
 
-export default How
+function How() {
+  return (
+    <VerticalTimeline>
+    {howItWorks.map((howItWorks, index) => (
+     <HowSteps key={index} howItWorks={howItWorks} />
+    ))}
+ </VerticalTimeline>
+  )
+}
+
+export default SectionWrapper(How, '')
