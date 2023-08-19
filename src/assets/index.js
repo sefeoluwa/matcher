5db5683cc7492c40576eef53e0a98eb9780fe1f3
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-19 07:24:18 +0100

    removed unused images

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,12 +1,10 @@
-import header from './header.png'
-import logo from './logo.jpg'
+import logo from './logo.svg'
 import menu from './menu.svg'
 import close from './close.svg'
 import giphy from './giphy.gif'
 
 export {
     logo,
-    header,
     menu,
     close,
     giphy,
