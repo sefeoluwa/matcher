34b3ba9908d0b5e360d32dd6edba0cc4e3fd3d8c
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-19 23:22:27 +0100

    some weird looking animation for cards that's actually nice :tada:

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+/* eslint-disable react/no-unknown-property */
 import { motion } from "framer-motion";
-import "react-vertical-timeline-component";
 import { SectionWrapper } from '../hoc';
-import { features } from '../constants'
+import { features } from '../constants';
+import { Tilt } from 'react-tilt';
+import { fadeIn } from '../utils/motion';
+
+const FeaturesCard = () => {
+  return (
+    <motion.div className="featuresCardContainer flex justify-center gap-10 pt-10">
+      {features.map((feat, index) => (
+        <Tilt key={feat.title} className="w-full">
+          <motion.div 
+            options={{ 
+              max:20,
+              scale:1,
+              speed:450
+              }}
+              variants={fadeIn("right", "spring", 0.2 * index, 0.75)} 
+            className="features-card">
+            <img src={feat.icon} alt="" />
+            <h3 className="text-[25px] font-bold">{feat.title}</h3>
+            <p className="text-[18px]">{feat.content}</p>
+          </motion.div>
+        </Tilt>
+      ))}
+    </motion.div>
+  );
+};
+
+
+
+
+
 
 function Features() {
   return (
@@ -10,15 +39,8 @@ function Features() {
     <div className="heading">
       <h2 className='font-bold text-[30px]'>Powerful features to help you pass the screening stage amd get hired faster.</h2>
     </div>
-    <div className="flex flex-wrap justify-around w-full">
-      {features.map((feat) => (
-        <div className="features-card mt-5" key={feat.title}>
-          <img src={feat.icon} alt="" />
-          <h3 className='text-[25px] font-bold '>{feat.title}</h3>
-          <p className='text-[18px]'>{feat.content}</p>
-        </div>
-      ))}
-    </div>
+
+    <FeaturesCard />
     </>
   )
 }
