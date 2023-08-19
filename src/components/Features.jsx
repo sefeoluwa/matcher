5997addffd3e3c5219e7049ac5879ccbc8b53aca
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-19 07:28:44 +0100

    rendered features content with map

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { motion } from "framer-motion";
+import "react-vertical-timeline-component";
+import { SectionWrapper } from '../hoc';
+import { features } from '../constants'
 
 function Features() {
   return (
-    <div>Features</div>
+    <>
+    <div className="heading">
+      <h2>Powerful features to help you pass the screening stage amd get hired faster.</h2>
+    </div>
+    <div className="">
+      {features.map((feat) => (
+        <div className="" key={feat.title}>
+          <img src={feat.icon} alt="" />
+          <h3>{feat.title}</h3>
+          <p>{feat.content}</p>
+        </div>
+      ))}
+    </div>
+    </>
   )
 }
 
-export default Features
+export default SectionWrapper(Features, '')
