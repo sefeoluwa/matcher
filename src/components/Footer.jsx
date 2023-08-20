61de358ee908ca08aeaf5cd1ee74f2fc5addf9d4
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-20 23:28:01 +0100

    added footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <>
+    <footer>
+      <div className="">
+        <h3>Resume Matcher</h3>
+        <p>GitHub Apache 2.0</p>
+      </div>
+
+      <div className="">
+        <h2>Community</h2>
+        <p>DEV Community Post</p>
+      </div>
+
+      <div className="">
+        <h3>Find Us</h3>
+        <ul>
+          <li>Product Hunt</li>
+          <li>Discord</li>
+          <li>Alternative To</li>
+          <li>Toolify AI</li>
+        </ul>
+      </div>
+
+      <div className="">
+        <h3>GitHub</h3>
+        <ul>
+          <li>Creator</li>
+          <li>Contributors</li>
+          <li>Star Gazers</li>
+          <li>Forks</li>
+        </ul>
+      </div>
+    </footer>
+    </>
   )
 }
 
