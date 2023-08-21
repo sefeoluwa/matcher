803e1a48b9b912b851930364669dcfa61f8414ae
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-21 11:23:20 +0100

    added new images

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,8 @@
 import {
   scanner,
   pdf,
-  launch
+  launch,
+  download
 
 } from '../assets';
 
@@ -40,27 +41,27 @@ const features = [
 
 const howItWorks = [
   {
-    icon: '',
+    iconBg: download,
     title: 'Step 1',
     content: 'Upload your resume.'
   },
   {
-    icon: '',
+    iconBg: download,
     title: 'Step 2',
     content: 'Enter job description.'
   },
   {
-    icon: '',
+      iconBg: download,
     title: 'Step 3',
     content: 'AI powered analytics and resume review.'
   },
   {
-    icon: '',
+      iconBg: download,
     title: 'Step 4',
     content: 'Algorithm matches resume to job description.'
   },
   {
-    icon: '',
+      iconBg: download,
     title: 'Step 5',
     content: 'Download optimized resume.'
   },
