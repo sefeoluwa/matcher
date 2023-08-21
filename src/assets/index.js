7e4fb3292afcd4e3ff8bbff9bff26e8b957c5d7d
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-21 10:25:30 +0100

    new assets added

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,10 +2,18 @@ import logo from './logo.svg'
 import menu from './menu.svg'
 import close from './close.svg'
 import giphy from './giphy.gif'
+import download from './download.gif'
+import launch from './launch.png'
+import pdf from './pdf.png'
+import scanner from './scanner.webp'
 
 export {
     logo,
     menu,
     close,
     giphy,
+    download,
+    launch,
+    pdf,
+    scanner,
 }
