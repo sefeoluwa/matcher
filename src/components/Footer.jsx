875dd0ed19925ab41f6dfbe1e8118cc54d5679e0
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-22 13:06:55 +0100

    styles for footer elements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { FaGithub, FaDiscord, FaTwitter } from 'react-icons/fa';
 
 
 function Footer() {
   return (
     <>
-    <footer className="flex justify-between h-[35vh] p-10 border-t-[1px] border-sky-100">
-      <div className="">
+    <footer className="flex flex-col  gap-20 justify-between h-[35vh] p-10 border-t-[1px] border-sky-100">
+     <div className="flex justify-between flex-wrap gap-12">
+     <div className="">
         <h3 className="font-extrabold text-[16px]">Resume Matcher</h3>
         <p>GitHub Apache 2.0</p>
       </div>
@@ -24,7 +28,7 @@ function Footer() {
         </ul>
       </div>
 
-      <div className="">
+      <div className="git-div">
         <h3 className="font-extrabold text-[16px]">GitHub</h3>
         <ul>
           <li>Creator</li>
@@ -33,6 +37,17 @@ function Footer() {
           <li>Forks</li>
         </ul>
       </div>
+     </div>
+
+     <div className="flex justify-between flex-wrap gap-6 pb-5">
+      <h2> <span className="text-sky-600">Apache 2.0 License</span> © Resume Matcher Contributors</h2>
+      <div className="flex justify-between w-[150px]">
+      <FontAwesomeIcon icon={faEnvelope} className='cursor-pointer'/>
+      <FaGithub className='cursor-pointer'/>
+      <FaDiscord className='cursor-pointer'/>
+      <FaTwitter className='cursor-pointer'/>
+      </div>
+     </div>
     </footer>
     </>
   )
