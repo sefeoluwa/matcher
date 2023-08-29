e96207207da9226a57946e11f81bdf78b9c179bb
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-29 08:44:52 +0100

    removed Tilt

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -3,7 +3,6 @@
 import { motion } from "framer-motion";
 import { SectionWrapper } from '../hoc';
 import { features } from '../constants';
-import { Tilt } from 'react-tilt';
 import { fadeIn } from '../utils/motion';
 import { styles } from "../styles";
 
@@ -11,22 +10,21 @@ const FeaturesCard = () => {
   return (
     <motion.div className="featuresCardContainer flex justify-center gap-10 pt-10">
       {features.map((feat, index) => (
-        <Tilt key={feat.title} className="w-full">
           <motion.div 
+          key={feat.title}
             options={{ 
               max:20,
               scale:1,
               speed:450
               }}
               variants={fadeIn("right", "spring", 0.2 * index, 0.75)} 
-            className="features-card h-auto">
+            className="features-card h-auto w-full">
             <div className="featuresIcon">
             <img src={feat.icon} alt="" />
             </div>
             <h3 className="text-[25px] font-bold">{feat.title}</h3>
             <p className="text-[18px]">{feat.content}</p>
           </motion.div>
-        </Tilt>
       ))}
     </motion.div>
   );
