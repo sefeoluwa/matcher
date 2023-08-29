f0d4236f344dfa7af039e2481765ee2d0ad73f3a
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-29 09:25:48 +0100

    removed verticalTimeline, added plain text with map

--- src/components/How.jsx
+++ src/components/How.jsx
@@ -1,37 +1,25 @@
 /* eslint-disable react/prop-types */
 import { SectionWrapper } from '../hoc'
-import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
-import "react-vertical-timeline-component/style.min.css";
 import { styles } from '../styles';
 import { howItWorks } from '../constants';
 import { textVariant } from "../utils/motion";
 
-const HowSteps = ({ howItWorks }) => {
-  return (
-    <VerticalTimelineElement 
-    contentStyle={{background: '#050827', color: '#fff'}}
-    icon={
-      <div className="flex justify-center items-center w-full h-full bg-primary rounded-full">
-        <img src={howItWorks.iconBg} alt= "steps animated arrow" className="w-[60%] h-[60%] object-contain"/>
-      </div>
-   }
-    >
-       <div>
-          <img src={howItWorks.icon} alt="" />
-           <h3 >{howItWorks.title}</h3>
-           <p>{howItWorks.content}</p>
-         </div>
-       </VerticalTimelineElement>
-  )
-}
 
 function How() {
   return (
-    <VerticalTimeline>
-    {howItWorks.map((howItWorks, index) => (
-     <HowSteps key={index} howItWorks={howItWorks} />
-    ))}
- </VerticalTimeline>
+    <>
+    <h2 className={`${styles.sectionHeadText} text-center`}>How it works</h2>
+
+    <div className=" flex flex-wrap justify-between">
+      {howItWorks.map((how) => (
+        <div key={how.num} className='p-4'>
+          <p>{how.num}</p>
+          <h3 className='w-[200px]'>{how.title}</h3>
+          <p className='how-content'>{how.content}</p>
+        </div>
+      ))}
+    </div>
+    </>
   )
 }
 
