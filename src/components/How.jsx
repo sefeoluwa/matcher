35d85d7ceff10d7479ac6fc4df193bc62e3e9ee3
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-29 10:07:51 +0100

    styles for text content

--- src/components/How.jsx
+++ src/components/How.jsx
@@ -8,14 +8,14 @@ import { textVariant } from "../utils/motion";
 function How() {
   return (
     <>
-    <h2 className={`${styles.sectionHeadText} text-center`}>How it works</h2>
+    <h2 className={`${styles.sectionHeadText} text-center mt-10 mb-10`}>How it works</h2>
 
-    <div className=" flex flex-wrap justify-between">
+    <div className=" flex flex-wrap justify-between gap-2">
       {howItWorks.map((how) => (
         <div key={how.num} className='p-4'>
-          <p>{how.num}</p>
-          <h3 className='w-[200px]'>{how.title}</h3>
-          <p className='how-content'>{how.content}</p>
+          <p className='text-[#209cff]'>{how.num}</p>
+          <h3 className='w-[220px] font-bold text-[19px]'>{how.title}</h3>
+          <p className='how-content text-[16px]'>{how.content}</p>
         </div>
       ))}
     </div>
