a4d38f9d2cc51a90578f619180bae2bc533d979c
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-30 11:01:34 +0100

    chnaged pricing card design

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -36,16 +36,16 @@ useEffect (() => {
             <div 
           data-aos="flip-up"
           data-aos-duration="2000"
-          className='pricingCard w-[330px] h-[441px] bg-[#F7F7FB] p-[1vw] rounded-xl flex flex-col items-center pt-10 '
+          className='pricingCard  w-[330px] h-[441px] bg-gray-900 p-[1vw] rounded-xl flex flex-col items-center pt-10 '
           >
             <p className='text[18px] leading-[28px] tracking-[-0.56px] pb-6'>{plan.title}</p>
             <h3 className='text-white text-[50px] leading-[56px] tracking-[-1.03px] pb-6'>{plan.price}</h3>
             <ul className='flex flex-col'>
             {plan.specs.map((spec) => ( 
-            <li key={spec} className='text-[#CFD4D43] text[17px] leading-[50px] tracking-[-0.53px] text-center'>{spec}</li>
+            <li key={spec} className='text-[#CFD4D43] text[17px] leading-[50px] tracking-[-0.53px] text-center cursor-default'>{spec}</li>
           ))}
             </ul>
-            <button className='text-white bg-secondary w-[285px] h-[60px] text[18px] leading-[28px] tracking-[-0.56px] font-bold rounded-[12px] mt-6' >{plan.button}</button>
+            <button className='text-white border-[1px] border-opacity-50 w-[285px] h-[50px] text[18px] leading-[28px] tracking-[-0.56px] font-bold rounded-[12px] mt-6 hover:bg-secondary hover:border-none' >{plan.button}</button>
           </div>
           </div>
         ))}
