f73837eb1de899d7b258fd112817e3149dc746c8
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-31 17:08:43 +0100

    added new pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { Features, Footer, Hero, How, Navbar, Pricing, Team } from '../components'
+
+function Home() {
+  return (
+   <>
+     <Navbar />
+      <Hero />
+      <Features />
+      <How />
+      <Pricing />
+      <Team />
+      <Footer />
+   </>
+  )
+}
+
+export default Home
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from './Home'
+import Login from './Login'
+import CreatePost from './CreatePost'
+
+export {
+    Home,
+    Login,
+    CreatePost,
+
+}
