503486398c3c39033a90b3c93a778027307c1847
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-31 17:40:21 +0100

    nav and footer now constant

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 import { Home, CreatePost, Login } from './pages'
 
+import { Navbar, Footer } from "./homeComponents"
+
 
 
 function App() {
 
   return (
     <Router>
+      <Navbar />
       <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/createpost" element={<CreatePost />} />
           <Route path="/login" element={<Login />} />
       </Routes>
+      <Footer />
     </Router>
   )
 }
